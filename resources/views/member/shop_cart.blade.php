b31cf222d33686a609e92693b03296876e00f2e2
Mavis <llingchen.27@gmail.com>
2020-11-24 15:41:59 +0800

    改shopcartcontroller的store query builder

--- resources/views/member/shop_cart.blade.php
+++ resources/views/member/shop_cart.blade.php
@@ -2,33 +2,38 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h6>購物車</h6>
+        <h6 class="title-canter">購物車</h6>
 
         @if(isset($shopcartdata) AND count($shopcartdata) > 1)
-        <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">商品名稱</th>
-                <th scope="col">單項價格</th>
-                <th scope="col">數量</th>
-              </tr>
-            </thead>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">商品名稱</th>
+                    <th scope="col">單項價格</th>
+                    <th scope="col">數量</th>
+                </tr>
+                </thead>
 
-            <tbody>
-                @foreach($shopcartdata as $key)
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>{{ $key->name  }}</td>
-                        <td>$ {{ $key->price }}</td>
-                        <td>{{ $key->total_purchase_item }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
+                <tbody>
+                    @php($i = 1)
+                    @foreach($shopcartdata as $key)
+                        <tr>
+                            <th scope="row">{{ $i++ }}</th>
+                            <td>{{ $key->name  }}</td>
+                            <td>$ {{ $key->price }}</td>
+                            <td>{{ $key->total_purchase_item }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
 
-        </table>
+            </table>
         @else
-            <p>購物車還沒有東西喔</p>
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <p class="lead title-canter">你的購物車還沒有東西喔!!!</p>
+                </div>
+            </div>
         @endif
 
     </div>
